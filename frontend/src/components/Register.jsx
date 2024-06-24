231af2167c9f26bd1160eeab7489a4409e57c80e
Ashutosh Tripathi <63796943+ashu08tri@users.noreply.github.com>
2024-06-24 13:37:12 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -6,7 +6,7 @@ const Register = ({onClose, openLogin}) => {
 
     const handleRegister = async (e) => {
         e.preventDefault();
-        const response = await fetch('http://localhost:4000/register', {
+        const response = await fetch('https://daiv-prashna.onrender.com/register', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
