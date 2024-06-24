84942cc523c7b749495321c76d93d90116f278df
Ashutosh Tripathi <63796943+ashu08tri@users.noreply.github.com>
2024-06-24 13:36:57 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,7 +8,7 @@ const Login = ({onClose}) => {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        const response = await fetch('http://localhost:4000/login', {
+        const response = await fetch('https://daiv-prashna.onrender.com/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
